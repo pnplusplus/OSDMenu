ce1ad740d3a3afbfdc4b8e385e6a282c93f64975
Ivan V <ivv@i1v.org>
2025-03-30 17:36:05 +0300

    common: allow paths to be overriden

--- common/defaults.h
+++ common/defaults.h
@@ -4,8 +4,16 @@
 
 // All files must be placed on the memory card.
 // mc? paths are also supported.
+#ifndef CONF_PATH
 #define CONF_PATH "mc0:/SYS-CONF/OSDMENU.CNF"
+#endif
+
+#ifndef LAUNCHER_PATH
 #define LAUNCHER_PATH "mc0:/BOOT/launcher.elf"
+#endif
+
+#ifndef DKWDRV_PATH
 #define DKWDRV_PATH "mc0:/BOOT/DKWDRV.ELF"
+#endif
 
 #endif
